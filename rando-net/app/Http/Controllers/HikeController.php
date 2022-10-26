1b43d44f80265b9a53ba97437cd36f4616f761c3
guillaumemouchet <guillaume.mouchet@he-arc.ch>
2022-10-26 16:02:36 +0200

    Correction du code

--- rando-net/app/Http/Controllers/HikeController.php
+++ rando-net/app/Http/Controllers/HikeController.php
@@ -7,101 +7,106 @@ use Illuminate\Http\Request;
 
 class HikeController extends Controller
 {
+
+    /**
+     * Display all the Hikes.
+     *
+     * @return Response
+     */
     public function index()
     {
         $hikes = Hike::all();
-
         return view('hike.index', ['hikes' => $hikes]);
     }
 
-    public function show() //used for debug purpous
+    /**
+     * Display all the Hikes.
+     *
+     * @return Response
+     */
+    public function show()
     {
         $hikes = Hike::all();
 
         return view('hike.index', ['hikes' => $hikes]);
     }
+
+    /**
+     * Display form for creation.
+     *
+     * @return Response
+     */
     public function create()
     {
-        //See if authors or tags are needed
         return view("hike.create");
     }
 
+    /**
+     * Edit the chosen hike in the Database
+     *
+     * @param Request $request
+     * @param  int  $id
+     */
     public function edit(Request $request, $id)
     {
-        //$hike = Hike::findOrFail($id)->update($request->all());
         $hike = Hike::findOrFail($id);
-        $request->validate([
-            'name' => 'required|min:5|max:50',
-            'region' => 'required|min:5|max:30',
-            'coordinates' => 'required|min:15|max:15',
-            'difficulty' => 'required|integer|gte:0|lte:5',
-            'map' => 'required',
-            'description' => 'required',
-        ]);
-
-        $hike->name = $request->name;
-        $hike->region = $request->region;
-        $hike->coordinates = $request->coordinates;
-        $hike->difficulty = $request->difficulty;
-        $hike->map = $request->map;
-        $hike->description = $request->description;
-        $hike->validated = True;
+        $request->validate($hike->request_validator());
+        $hike->modify($request,True);
 
         $hike->update();
-
-        return redirect()->route("admin.index");
     }
 
     /**
      * Delete the chosen hike
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public static function destroy($id)
     {
         $hike = Hike::findOrFail($id);
         $hike->delete();
-        return redirect()->route("admin.index");
     }
 
-
-    //l'appel au function destroy et edit ne fonctionne pas...
+    /**
+     * Determine what action the admin choosed
+     * He can edit and validate the Hike
+     * He can delete it and reject it
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
     public function update(Request $request, $id)
     {
-
+        $status = "";
         if ($_REQUEST['btnSubmit'] == "validate") {
             HikeController::edit($request,$id);
+            $status = "Admin validated hike successfully";
 
         } else if ($_REQUEST['btnSubmit'] == "reject") {
             HikeController::destroy($id);
+            $status = "Hike rejected successfully";
         }
-        return redirect()->route("admin.index");
+        return redirect()->route("admin.index")->with("success", $status);
     }
+
+    /**
+     * Store the Hike in the Database
+     *
+     *
+     * @param  Request  $request
+     * @return Response
+     */
 
     public function store(Request $request)
     {
-        $request->validate([
-            'name' => 'required|min:5|max:50',
-            'region' => 'required|min:5|max:30',
-            'coordinates' => 'required|min:15|max:15',
-            'difficulty' => 'required|integer|gte:0|lte:5',
-            'map' => 'required',
-            'description' => 'required',
-        ]);
-
         $hike = new Hike();
-        $hike->name = $request->name;
-        $hike->region = $request->region;
-        $hike->coordinates = $request->coordinates;
-        $hike->difficulty = $request->difficulty;
-        $hike->map = $request->map;
-        $hike->description = $request->description;
-        $hike->validated = False;
+        $request->validate($hike->request_validator());
 
+        $hike->modify($request,false);
 
         $hike->save();
 
-        return redirect()->route("hike.index"); // ->with("success", "Created successfully");
+        return redirect()->route("hike.index")->with("success", "Hike created successfully");
     }
 }
--- rando-net/app/Models/Hike.php
+++ rando-net/app/Models/Hike.php
@@ -4,9 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
 
 class Hike extends Model
 {
     use HasFactory;
-    
+    protected $fillable = [
+        "name", "region", "coordinates", "difficulty", "map", "description"
+    ];
+
+    function modify(Request $req, bool $validate)
+    {
+        $this->name = $req->name;
+        $this->region = $req->region;
+        $this->coordinates = $req->coordinates;
+        $this->difficulty = $req->difficulty;
+        $this->map = $req->map;
+        $this->description = $req->description;
+        $this->validated = $validate;
+    }
+
+    public function request_validator()
+    {
+        $req = [
+            'name' => 'required|min:5|max:50',
+            'region' => 'required|min:5|max:30',
+            'coordinates' => 'required|min:15|max:15',
+            'difficulty' => 'required|integer|gte:0|lte:5',
+            'map' => 'required',
+            'description' => 'required',
+        ];
+        return $req;
+    }
 }
