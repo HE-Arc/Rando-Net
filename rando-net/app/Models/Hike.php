2bdd49d9c87136d34587d2aed5bf5b574529ef22
WindRider97 <benjamin.mouchet@outlook.com>
2022-11-30 16:11:01 +0100

    fixed merge

--- rando-net/app/Models/Hike.php
+++ rando-net/app/Models/Hike.php
@@ -16,40 +16,4 @@ class Hike extends Model
         "name", "region", "coordinates", "difficulty", "map", "description"
     ];
 
-    /**
-     * Modify the hike locally
-     *
-     * @param Hike $this
-     * @param Request
-     * @param bool
-     */
-
-    function modify(Request $req, bool $validate)
-    {
-        $this->name = $req->name;
-        $this->region = $req->region;
-        $this->coordinates = $req->coordinates;
-        $this->difficulty = $req->difficulty;
-        $this->map = $req->map;
-        $this->description = $req->description;
-        $this->validated = $validate;
-    }
-
-    /**
-     * Create the validator for the forms
-     *
-     * @return Array
-     */
-    public function request_validator()
-    {
-        $req = [
-            'name' => 'required|min:5|max:50',
-            'region' => 'required|min:5|max:30',
-            'coordinates' => 'required|min:15|max:15',
-            'difficulty' => 'required|integer|gte:0|lte:5',
-            'map' => 'required',
-            'description' => 'required',
-        ];
-        return $req;
-    }
 }
