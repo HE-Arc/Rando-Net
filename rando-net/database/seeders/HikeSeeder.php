6d021e8fad62ddf832fcc64cd668e8aaee3762c9
Tan Dorian <dorian.tan@he-arc.ch>
2022-11-30 15:56:26 +0100

    rebase

--- rando-net/database/seeders/HikeSeeder.php
+++ rando-net/database/seeders/HikeSeeder.php
@@ -18,11 +18,8 @@ class HikeSeeder extends Seeder
         Hike::truncate();
         $hikes = [
             [
-<<<<<<< HEAD
-                "name" => "sMarche sur les crêtes de Tête de Ran",
-=======
+
                 "name" => "Marche sur les crêtes de Tête de Ran",
->>>>>>> d963283 (fixed merge conflict)
                 "region" => "Tête de Ran",
                 "coordinates" => '555\'844/211\'844',
                 "difficulty" => 2,
@@ -37,7 +34,7 @@ class HikeSeeder extends Seeder
                 "difficulty" => 3,
                 "map" => "todo",
                 "description" =>
-                    "Jolie vue sur le Matterhorn depuis le Stellisee",
+                "Jolie vue sur le Matterhorn depuis le Stellisee",
                 "validated" => true,
             ],
         ];
