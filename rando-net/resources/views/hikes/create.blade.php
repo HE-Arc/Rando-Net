b804e809244a0f592911e12dfbafd84ee11e6924
guillaumemouchet <guillaume.mouchet@he-arc.ch>
2022-12-14 14:52:03 +0100

    fixed merge conflict

--- rando-net/resources/views/hikes/create.blade.php
+++ rando-net/resources/views/hikes/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<form action="{{route("hikes.store")}}" method="POST">
+<form action="{{route("hikes.store")}}" enctype="multipart/form-data" method="POST">
     @csrf
     <div class="row">
         <div class="col-12 col-lg-6 offset-0 offset-lg-3">
@@ -67,8 +67,8 @@
                             </div>
 
                             <div class="form-group col-6">
-                                <label for="inputMap">Map</label> <!-- il faut faire que le user puisse ajouter une image-->
-                                <input type="text" name="map" class="form-control" id="inputMap" value="{{old('map')}}">
+                                <label for="inputMap">Map</label>
+                                <input type="file" name="image" class="form-control" id="inputMap">
                             </div>
 
                             <div class="form-group col-6">
