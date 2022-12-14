ffa3005acdea260ef4a82754ac3792d48ffea9d0
guillaumemouchet <guillaume.mouchet@he-arc.ch>
2022-12-14 14:42:10 +0100

    add all elements from hike

--- rando-net/resources/views/hikes/show.blade.php
+++ rando-net/resources/views/hikes/show.blade.php
@@ -16,7 +16,7 @@
             <!--{{$hike->map}}-->
             <img id="hikeimg" src="/assets/images/placeholder.jpg" class="float-left" alt="hike img">
             <div class="float-right">
-                <h5 class="display-4">Comments</h5>
+                <h2 class="display-6">Comments</h2>
                 <hr class="my-4">
                 <p class="lead">WORK IN PROGRESS</p>
             </div>
@@ -24,8 +24,18 @@
 
         <p> DISPLAY LIST OF TAGS</p>
         <hr class="my-4">
-
-        <p class="lead">{{$hike->description}}</p>
+        <div class="m-5">
+            <h1 class="display-6">Description</h1>
+            <p class="lead">{{$hike->description}}</p>
+        </div>
+        <div class="d-flex justify-content-between">
+            <div class="container">
+                <p class="lead">Region : {{$hike->region}}</p>
+            </div>
+            <div class="container">
+                <p class="lead">Coordinates : {{$hike->coordinates}}</p>
+            </div>
+        </div>
     </div>
 
 @endsection
