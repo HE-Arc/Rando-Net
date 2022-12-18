8f8a2355a65aa66591e79e5b10b7a95005211f0e
guillaumemouchet <guillaume.mouchet@he-arc.ch>
2022-12-18 14:20:15 +0100

    add jumbotron in profil and show research

--- rando-net/resources/views/hikes/user_hikes.blade.php
+++ rando-net/resources/views/hikes/user_hikes.blade.php
@@ -1,22 +1,24 @@
 @extends('layout.app')
 
 @section('content')
-    <h1>Your submitted hikes</h1>
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Region</th>
-                <th scope="col">Difficulty</th>
-            </tr>
-        </thead>
+    <div class="jumbotron">
+        <h1>Your submitted hikes</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Region</th>
+                    <th scope="col">Difficulty</th>
+                </tr>
+            </thead>
 
-        @foreach ($hikes as $hike)
-            <tr id="hike" onclick="window.location='{{ route('hikes.show', ['hike' => $hike]) }}'">
-                <td>{{ $hike->name }}</td>
-                <td>{{ $hike->region }}</td>
-                <td>{{ $hike->difficulty }}</td>
-            </tr>
-        @endforeach
-    </table>
+            @foreach ($hikes as $hike)
+                <tr id="hike" onclick="window.location='{{ route('hikes.show', ['hike' => $hike]) }}'">
+                    <td>{{ $hike->name }}</td>
+                    <td>{{ $hike->region }}</td>
+                    <td>{{ $hike->difficulty }}</td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
 @endsection
--- rando-net/resources/views/tags/show.blade.php
+++ rando-net/resources/views/tags/show.blade.php
@@ -1,27 +1,30 @@
 @extends('layout.app')
 
 @section('content')
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Region</th>
-                <th scope="col">Difficulty</th>
-                <th scope="col">Tag</th>
-            </tr>
-        </thead>
+    <div class="jumbotron">
 
-        @foreach ($hikes as $hike)
-            <tr id="hike" onclick="window.location='{{ route('hikes.show', ['hike' => $hike]) }}'">
-                <td>{{ $hike->name }}</td>
-                <td>{{ $hike->region }}</td>
-                <td>{{ $hike->difficulty }}</td>
-                <td>
-                    @foreach ($hike->tags as $tag)
-                        {{ $tag->name }}
-                    @endforeach
-                </td>
-            </tr>
-        @endforeach
-    </table>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Region</th>
+                    <th scope="col">Difficulty</th>
+                    <th scope="col">Tag</th>
+                </tr>
+            </thead>
+
+            @foreach ($hikes as $hike)
+                <tr id="hike" onclick="window.location='{{ route('hikes.show', ['hike' => $hike]) }}'">
+                    <td>{{ $hike->name }}</td>
+                    <td>{{ $hike->region }}</td>
+                    <td>{{ $hike->difficulty }}</td>
+                    <td>
+                        @foreach ($hike->tags as $tag)
+                            {{ $tag->name }}
+                        @endforeach
+                    </td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
 @endsection
