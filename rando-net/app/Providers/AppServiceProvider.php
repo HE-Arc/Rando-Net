24ae20dc747b17e5ad45cdff39dfbc6e8dcbc421
WindRider97 <benjamin.mouchet@outlook.com>
2022-12-19 15:38:31 +0100

    fixed mixed-content issue

--- rando-net/app/Providers/AppServiceProvider.php
+++ rando-net/app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
         Paginator::useBootstrapFour();
     }
 }
